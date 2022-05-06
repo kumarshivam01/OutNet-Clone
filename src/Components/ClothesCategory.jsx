ca529ea6d0118a78d56f497ab61dbdeaf0f74b03
kumarshivam01 <kumarshivamsiitpatna@gmail.com>
2022-05-06 10:19:22 +0530

    done

--- src/Components/ClothesCategory.jsx
+++ src/Components/ClothesCategory.jsx
@@ -8,21 +8,21 @@ export default function ClothesCategory(props) {
         <div className="row">
           <div className="col">
             <img src={props.jacket} alt="" />
-            <p style={{fontSize:"18px", textAlign:"center",marginTop:"25px"}} classNameName='para-1'>{props.para1}</p>
+            <p style={{ fontSize: "18px", textAlign: "center", marginTop: "25px" }} classNameName='para-1'>{props.para1}</p>
             <p classNameName='para-2'>{props.para2}</p>
-            <button style={{border:"none", padding:"15px",border:"1px solid black",}}>{props.button1}</button>
+            <button style={{ border: "none", padding: "15px", border: "1px solid black", }}>{props.button1}</button>
           </div>
           <div className="col">
             <img src={props.shirts} alt="" />
-            <p style={{fontSize:"18px", textAlign:"center",marginTop:"25px"}} classNameName='para-1'>{props.para3}</p>
+            <p style={{ fontSize: "18px", textAlign: "center", marginTop: "25px" }} classNameName='para-1'>{props.para3}</p>
             <p classNameName='para-2'>{props.para4}</p>
-            <button style={{border:"none", padding:"15px",border:"1px solid black",}}>{props.button2}</button>
+            <button style={{ border: "none", padding: "15px", border: "1px solid black", }}>{props.button2}</button>
           </div>
           <div className="col">
             <img src={props.paint} alt="" />
-            <p style={{fontSize:"18px", textAlign:"center",marginTop:"25px"}} classNameName='para-1'>{props.para5}</p>
+            <p style={{ fontSize: "18px", textAlign: "center", marginTop: "25px" }} classNameName='para-1'>{props.para5}</p>
             <p classNameName='para-2'>{props.para6}</p>
-            <button style={{border:"none", padding:"15px",border:"1px solid black",}}><Link to="/men/clothing">{props.button3}</Link></button>
+            <button style={{ border: "none", padding: "15px", border: "1px solid black", }}><Link to="/men/clothing">{props.button3}</Link></button>
           </div>
         </div>
       </div>
