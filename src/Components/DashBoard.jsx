197d9efbb099083941b21b76babc21dd11d5966f
Gyanmaurya <gyanprakashme0051@gmail.com>
2022-05-06 14:08:35 +0530

    DashBoard
    done

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -1,5 +1,12 @@
 
 import "../Style/DashBoard.css"
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AddCardIcon from '@mui/icons-material/AddCard';
+import LoyaltyIcon from '@mui/icons-material/Loyalty';
+import EmailIcon from '@mui/icons-material/Email';
 const name="GYAN"
 export const DashBoard=()=>{
 
@@ -12,17 +19,20 @@ export const DashBoard=()=>{
 <h1>Welcome to your Account</h1>
 <div className="dashBoardContainer">
 
-    <div>
-        Track My Order
+    <div className="dashmin"> 
+    <span><AddShoppingCartIcon/></span> <span>My Order</span>
+        
         <p>Track Your order or create a return</p>
 
     </div>
-    <div>Wish List</div>
-    <div>Address Book</div>
-    <div>Account Details</div>
-    <div>Saved Cards</div>
-    <div>Store Credit</div>
-    <div>Marketing Preferences</div>
+    <div className="dashmin"><span><FavoriteIcon/></span> <span>Wish List</span>
+    <p>Save the items you love for later and make sure you don't miss out !</p>
+    </div>
+    <div className="dashmin"><span><HomeIcon/></span> <span>Address Book</span> <p>Manage your addresses</p> </div>
+    <div className="dashmin"><span><AccountCircleIcon/></span> <span>Account Details</span> <p>Change your sign in information</p></div>
+    <div className="dashmin"><span><AddCardIcon/></span> <span>Saved Cards</span> <p>View and delete your payment details</p> </div>
+    <div className="dashmin"><span><LoyaltyIcon/></span> <span>Store Credit</span> <p>Check your balance and transactions</p> </div>
+    <div className="dashmin"><span><EmailIcon/></span> <span>Marketing Preferences</span> <p>Tailor your email from us</p> </div>
 
 </div>
 
